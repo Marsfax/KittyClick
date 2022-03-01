51246bb4228cc5917b11fd9aceb060749f2df7fe
Marsfax <marsfaxfaq@gmail.com>
2022-03-01 23:13:22 +0300

    SceneStore,Cotoutines

--- Assets/Scripts/StoreScene.cs
+++ Assets/Scripts/StoreScene.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-public class Achivestotal : MonoBehaviour
+public class StoreScene : MonoBehaviour
 {
     //public int total_money;
 
